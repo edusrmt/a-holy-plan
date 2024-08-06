d077b9fa28da7a3895e5d1642c1928c3831edf83
Eduardo Sarmento <cealvesarmento@gmail.com>
2024-08-06 12:15:32 -0300

    Adds Vercel Analytics and Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { Home } from "./pages/Home/index.tsx";
 import { GlobalStyle } from "./global.ts";
 import { Plan } from "./pages/Plan/index.tsx";
 import { BiblePlanProvider } from "./contexts/BiblePlanContext.tsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const router = createBrowserRouter([
   {
@@ -23,5 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BiblePlanProvider>
       <RouterProvider router={router} />
     </BiblePlanProvider>
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
