020ea59e446bd6e16289acaf0a9644fe65c5255b
Eduardo Sarmento <cealvesarmento@gmail.com>
2024-09-23 12:26:47 -0300

    Changes credits link to Instagram

--- src/pages/Plan/index.tsx
+++ src/pages/Plan/index.tsx
@@ -96,7 +96,7 @@ export const Plan = () => {
             <p>
               Desenvolvido por{' '}
               <a
-                href="https://www.linkedin.com/in/edu-sarmento/"
+                href="https://www.instagram.com/edusrmt"
                 target="_blank"
                 rel="noopener noreferrer"
               >
